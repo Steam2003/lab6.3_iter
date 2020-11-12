57fa7d988fb3659cfd04d94c2b8ef03b3f66694e
Ivan Patiuk <gendalf2003@gmail.com>
2020-11-12 13:58:46 +0200

    lab6.3_iter_fix

--- lab6.3_iter.cpp
+++ lab6.3_iter.cpp
@@ -4,46 +4,11 @@
 
 using namespace std;
 
+void create(int* a, int Low, int Max, int N);
+void print(int* a, int N);
 template <typename T>
-T count1(T* a, T N)
-{
-	int j = 0;
-	for (int i = 0; i <= N-1; ++i)
-	{
-		if (a[i] % 2 == 0)
-		{
-			++j;
-		}
-	}
-	return j;
-}
-void print(int* a, int N)
-{
-	for (int i =0; i<=N-1; ++i)
-		cout << setw(3) << a[i] << " ";
+T count1(T* a, T N);
 
-}
-void create(int* a, int Low, int Max, int N)
-{
-	for (int i = 0; i <= N - 1; ++i)
-	{
-		a[i] = Low + rand() % (Max - Low + 1);
-	}
-	print(a, N);
-}
-template <typename T>
-T unittest(T c[], T i, T N, T j)
-{
-	if (i <= N - 1)
-	{
-		if (c[i] % 2 == 0)
-			++j;
-		return unittest(c, ++i, N, j);
-	}
-	cout << "------------------------------------------------------" << endl;
-	cout << "Unit test result (Кількість парних чисел в масиві): " << j << endl;
-	return j;
-}
 int main()
 {
 	int N, Low, Max;
@@ -56,9 +21,31 @@ int main()
 	cout << "Введіть максимальне значення елементу масиву: "; cin >> Max;
 	int* a = new int[N];
 	create(a, Low, Max, N);
+	print(a,N);
 	cout << endl;
 	cout << "Кількіть парних: " << count1(a, N) << endl;
 	delete[] a;
-	int c[10] = { -10,25,7,-15,17,3,1,0,4,30 };
-	unittest(c, 0, 10, 0);
+}
+void create(int* a, int Low, int Max, int N)
+{
+	for (int i = 0; i <= N - 1; ++i)
+		a[i] = Low + rand() % (Max - Low + 1);
+}
+void print(int* a, int N)
+{
+	for (int i = 0; i <= N - 1; ++i)
+		cout << setw(3) << a[i] << " ";
+}
+template <typename T>
+T count1(T* a, T N)
+{
+	int j = 0;
+	for (int i = 0; i <= N - 1; ++i)
+	{
+		if (a[i] % 2 == 0)
+		{
+			++j;
+		}
+	}
+	return j;
 }
